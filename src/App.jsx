6b0a6635093090daccb23506edfe5a8437cd5de8
M Kailasnath <kailasm917@gmail.com>
2026-09-22 15:10:12 +0530

    contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import ProjectCard from "./components/projectCard/projectCard";
 import PageContainer from "./components/pageContainer/pageContainer";
 import Navbar from "./components/navbar/Navbar";
+import { MoveUpRight } from "lucide-react";
 function App() {
   return (
     <div>
@@ -11,35 +12,56 @@ function App() {
 
         <main className="landingContainer" id="home">
           <p className="landingText">
-            I am a developer who <span>loves</span>
+            I am a developer who <span id="highlight">loves</span>
             <br />
             turning dreams into reality
           </p>
         </main>
         {/* about zection */}
-        <section className="landingContainer" id="about">
+        <section className="sectionContainer" id="about">
           <p className="landingText">
-            I am a developer who <span>loves</span>
+            I am a developer who <span  id="highlight">loves</span>
             <br />
             turning dreams into reality
           </p>
         </section>
         {/* projects zection */}
-         <section className="landingContainer" id="project">
+        <section className="sectionContainer" id="project">
           <p className="landingText">
-            I am a developer who <span>loves</span>
+            I am a developer who <span  id="highlight">loves</span>
             <br />
             turning dreams into reality
           </p>
         </section>
         {/* contact section */}
-         <section className="landingContainer" id="contact">
-          <p className="contactTitle">contact me</p>
-          <p className="landingText">
-            I am a developer who <span>loves</span>
-            <br />
-            turning dreams into reality
-          </p>
+        <section className="sectionContainer" id="contact">
+          <p className="sectionTitle">contact me</p>
+          <div className="contactSub">
+            <img src="./src/assets/github-profile.png" alt="profile" />
+            <ul className="navLi">
+              <li>
+                <a href="#home">
+                  <span>
+                    github <MoveUpRight />
+                  </span>
+                  {/* github */}
+                </a>
+              </li>
+              <li>
+                <a href="#about" target="_blank" rel="noopener">
+                  <span>kailasm917@gmail.com</span>
+                </a>
+              </li>
+              <li>
+                <a href="#project">
+                  <span>
+                    linkedin <MoveUpRight />
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <p className="footer">2026 M Kailasnath. All rights reserved</p>
         </section>
       </PageContainer>
     </div>
